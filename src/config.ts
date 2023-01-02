51dfeaa3c2030be442b15bded99ffa1ac6d1b8aa
Aymen Hammami <hammami.aym@outlook.com>
2023-01-02 01:43:37 +0100

    added league patch

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const PREFIX: string = "<";
 export const TOKEN: string = process.env.TOKEN;
 export const RIOT_TOKEN: string = process.env.RIOT_TOKEN;
+export const LEAGUE_OF_LEGENDS_PATCH: string = "12.23.1";
